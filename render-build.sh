fa07da59d2e3c78acb2f9be7a6c34ce957572471
Devansh Khatri <134623273+prodk123@users.noreply.github.com>
2025-03-06 09:18:44 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-apt-get update && apt-get install -y stockfish
+# Download Stockfish binary
+mkdir -p stockfish
+curl -L -o stockfish/stockfish https://stockfishchess.org/files/stockfish_15.1_linux_x64.zip
+unzip stockfish/stockfish -d stockfish/
+chmod +x stockfish/stockfish
+chmod +x render-build.sh

37c39d0305d4197e9fa15257c4e9051b41ef2491
Devansh Khatri <134623273+prodk123@users.noreply.github.com>
2025-03-06 09:42:34 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-# Create directory for Stockfish
+
+# Create stockfish directory
 mkdir -p stockfish
 
-# Download the correct Stockfish Linux binary
-curl -L -o stockfish/stockfish https://api.github.com/repos/official-stockfish/Stockfish/releases/latest | grep "browser_download_url.*stockfish-linux-x86-64" | cut -d '"' -f 4 | wget -O stockfish/stockfish -i -
+# Download the Stockfish Linux binary (precompiled)
+curl -L -o stockfish/stockfish https://stockfishchess.org/files/latest/linux/stockfish_15.1_linux_x64.zip
 
-# Make it executable
+# Unzip and move to stockfish folder
+unzip stockfish/stockfish_15.1_linux_x64.zip -d stockfish/
+
+# Give execute permissions
 chmod +x stockfish/stockfish

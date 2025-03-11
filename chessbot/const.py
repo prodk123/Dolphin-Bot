17988822e39f88856fb333bc93e27620b2a2d109
Devansh Khatri <134623273+prodk123@users.noreply.github.com>
2025-03-11 13:47:40 +0530

    Delete chessbot directory

--- chessbot/const.py
+++
@@ -1,8 +0,0 @@
-# Screen dimensions
-WIDTH = 900  # Increase width to fit score panel
-HEIGHT = 700  # Keep height the same
-
-# Board dimensions
-ROWS = 8
-COLS = 8
-SQSIZE = HEIGHT // ROWS  # Adjust square size to maintain aspect ratio

9d4bc7af716cd315aaa3314a378cbed8d95f4791
Jash Kapadia <jashmkapadia0@gmail.com>
2025-06-04 18:42:04 +0530

    Update board.py

--- chessbot/board.py
+++ chessbot/board.py
@@ -1,7 +1,7 @@
-from const import *
-from square import Square
-from piece import *
-from move import Move
+from chessbot.const import *
+from chessbot.square import Square
+from chessbot.piece import *
+from chessbot.move import Move
 import copy
 import os
 
